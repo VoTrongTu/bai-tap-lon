b57f6fab7a05cfe9096dd74a6cbec87ee7cba8c3
anhquoc <2451052285tu@ou.edu.vn>
2025-08-19 00:58:14 +0700

    update

--- js/style.js
+++ js/style.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // --------------Hiệu ừng tahr tim--------------
 document.addEventListener("DOMContentLoaded", () => {
     const heart = document.querySelector(".heart"); // lấy theo class
@@ -11,17 +10,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-=======
-// --------------Hiệu ừng tahr tim--------------
-document.addEventListener("DOMContentLoaded", () => {
-    const heart = document.querySelector(".heart"); // lấy theo class
-
-    heart.addEventListener("click", () => {
-        if (heart.src.includes("heart-red.png")) {
-            heart.src = "img/heart.png";   // tim rỗng
-        } else {
-            heart.src = "img/heart-red.png"; // tim đỏ
-        }
-    });
-});
->>>>>>> cf88349802c869466de3f67475e71c1389fd74be

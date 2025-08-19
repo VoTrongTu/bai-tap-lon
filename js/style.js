88a02cf7ec556baee5d9338228d78b6cbeede123
anhquoc <2451052285tu@ou.edu.vn>
2025-08-19 10:32:16 +0700

    update

--- js/style.js
+++ js/style.js
@@ -1,6 +1,18 @@
-// --------------Hiệu ừng tahr tim--------------
+// --------------Hiệu ứng thả tim--------------
 document.addEventListener("DOMContentLoaded", () => {
-    const heart = document.querySelector(".heart"); // lấy theo class
+    const heart = document.querySelector(".heart-Catalog1"); // lấy theo class
+
+    heart.addEventListener("click", () => {
+        if (heart.src.includes("heart-red.png")) {
+            heart.src = "img/heart.png";   // tim rỗng
+        } else {
+            heart.src = "img/heart-red.png"; // tim đỏ
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const heart = document.querySelector(".heart-Catalog2"); // lấy theo class
 
     heart.addEventListener("click", () => {
         if (heart.src.includes("heart-red.png")) {

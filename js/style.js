c5cc3d55e08ef7fbcecba8ddcdd7b0aee87391d3
anhquoc <2451052285tu@ou.edu.vn>
2025-08-19 11:59:17 +0700

    update

--- js/style.js
+++ js/style.js
@@ -1,24 +1,14 @@
-// --------------Hiệu ứng thả tim--------------
+// --------------Hiệu ứng thả tim cho tất cả catalog--------------
 document.addEventListener("DOMContentLoaded", () => {
-    const heart = document.querySelector(".heart-Catalog1"); // lấy theo class
+    const hearts = document.querySelectorAll(".heart");
 
-    heart.addEventListener("click", () => {
-        if (heart.src.includes("heart-red.png")) {
-            heart.src = "img/heart.png";   // tim rỗng
-        } else {
-            heart.src = "img/heart-red.png"; // tim đỏ
-        }
-    });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    const heart = document.querySelector(".heart-Catalog2"); // lấy theo class
-
-    heart.addEventListener("click", () => {
-        if (heart.src.includes("heart-red.png")) {
-            heart.src = "img/heart.png";   // tim rỗng
-        } else {
-            heart.src = "img/heart-red.png"; // tim đỏ
-        }
+    hearts.forEach(heart => {
+        heart.addEventListener("click", () => {
+            if (heart.src.includes("heart-red.png")) {
+                heart.src = "img/heart.png";   // tim rỗng
+            } else {
+                heart.src = "img/heart-red.png"; // tim đỏ
+            }
+        });
     });
 });
